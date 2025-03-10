91ac91cd4553b76b5b1260a41c46de0174f57816
HeYaoDaDa <3050166231@qq.com>
2025-03-11 00:09:39 +0800

    init

---
+++ src/pages/LoadPage.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div id="load-page-root">
+    <h1>Loading...</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#load-page-root {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>

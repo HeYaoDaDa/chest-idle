d2cbfe470cc6b674ac0dda6615eed92a4ca0d9fb
HeYaoDaDa <3050166231@qq.com>
2025-03-11 23:57:03 +0800

    add modal box

---
+++ src/components/misc/ModalBox.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+const emit = defineEmits<{
+  close: []
+}>()
+</script>
+
+<template>
+  <teleport to="body">
+    <div id="modal-box">
+      <div id="modal-background" @click.stop.prevent="emit('close')"></div>
+      <div id="modal-content">
+        <slot></slot>
+      </div>
+    </div>
+  </teleport>
+</template>
+
+<style lang="scss">
+#modal-box {
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+
+  #modal-background {
+    height: 100%;
+    width: 100%;
+    background-color: black;
+    opacity: 0.8;
+  }
+
+  #modal-content {
+    position: absolute;
+    border-radius: 4px;
+    overflow: auto;
+
+    min-width: 300px;
+    max-width: 98%;
+    min-height: 100px;
+    max-height: 98%;
+
+    background-color: white;
+    box-shadow: 0 0 4px 4px hsla(0, 0%, 81.6%, 0.2823529412);
+  }
+}
+</style>
--- src/pages/SkillPage.vue
+++ src/pages/SkillPage.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+import ModalBox from '@/components/misc/ModalBox.vue';
+import { Amount } from '@/model/common/Amount';
+import type { GatheringArea } from '@/model/data/GatheringArea';
 import { useDataStore } from '@/stores/data';
+import { useNotificationStore } from '@/stores/notification';
 import { computed, ref } from 'vue';
 import { onBeforeRouteUpdate, useRoute } from 'vue-router';
 
@@ -11,14 +15,46 @@ onBeforeRouteUpdate(async (to) => {
 
 const dataStore = useDataStore();
 const areas = computed(() => dataStore.getGatheringAreasBySkillId(skillId.value));
+
+const notificationStore = useNotificationStore();
+const openArea = ref(undefined as GatheringArea | undefined);
+const amount = ref('∞')
+const allowStart = computed(() => Amount.verify(amount.value));
+function openModal(area: GatheringArea) {
+  openArea.value = area;
+}
+function closeModal() {
+  openArea.value = undefined;
+  amount.value = '∞';
+}
+function addAction(area: GatheringArea) {
+  notificationStore.notification('info', area.skill.getName() + ' | ' + area.getName());
+  openArea.value = undefined;
+}
 </script>
 
 <template>
   <div id="skill-area-root">
-    <div v-for="area in areas" :key="area.id">
+    <div v-for="area in areas" :key="area.id" @click="openModal(area)">
       <div>{{ area.getName() }}</div>
     </div>
   </div>
+  <ModalBox v-if="openArea" @close="closeModal">
+    <div id="area-modal-box">
+      <div>{{ openArea.getName() }}</div>
+      <div>{{ openArea.skill.getName() }}</div>
+      <div>{{ openArea.getDescription() }}</div>
+      <div>{{ openArea.baseTime / 1000 }}s</div>
+      <div v-for="loot, index in openArea.products" :key="index">
+        {{ loot.percentage }}% {{ loot.item.getName() }}: {{ loot.min }}-{{ loot.max }}
+      </div>
+      <div>
+        <input type="text" v-model="amount" />
+        <button @click="amount = '∞'">∞</button>
+      </div>
+      <button @click="addAction(openArea)" :disabled="!allowStart">Start</button>
+    </div>
+  </ModalBox>
 </template>
 
 <style lang="scss">
@@ -41,5 +77,12 @@ const areas = computed(() => dataStore.getGatheringAreasBySkillId(skillId.value)
     justify-content: center;
     align-items: center;
   }
+}
+
+#area-modal-box {
+  display: flex;
+  flex-flow: column nowrap;
+  padding: 8px;
+  gap: 4px;
 }
 </style>

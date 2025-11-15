bbdc849cf0cefcfc9cad9b899014ffe8b12580c7
HeYaoDaDa <3050166231@qq.com>
2025-11-15 13:28:55 +0800

    chore: remove modalBox compatibility directory

--- src/components/modalBox/ActionModalBox.tsx
+++
@@ -1,4 +0,0 @@
-export { default } from '@/components/modals/ActionModalBox'
-// Deprecated compatibility file. Moved to src/components/modals.
-// Keep this file as a lightweight compatibility re-export.
-export { default as __compat } from '@/components/modals/ActionModalBox'
--- src/components/modalBox/ActionQueueModal.tsx
+++
@@ -1,4 +0,0 @@
-export { default } from '@/components/modals/ActionQueueModal'
-// Deprecated compatibility file. Moved to src/components/modals.
-// Keep this file as a lightweight compatibility re-export.
-export { default as __compat } from '@/components/modals/ActionQueueModal'
